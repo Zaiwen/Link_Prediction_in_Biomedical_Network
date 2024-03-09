bb6f0d8cac06f4891b9d9388d044af60d70aee4e
hujunwei <104148715+HJW577X@users.noreply.github.com>
2024-03-09 22:20:57 +0800

    Add files via upload

---
+++ code/openhgnn/layers/__init__.py
@@ -0,0 +1,5 @@
+from .HeteroLinear import GeneralLinear, HeteroLinearLayer
+from .HeteroLinear import HeteroMLPLayer, HeteroFeature
+from .MetapathConv import MetapathConv
+from .macro_layer import *
+from .micro_layer import *
